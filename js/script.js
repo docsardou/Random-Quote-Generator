e3a48fcd96378ca0bcb48de5a95baf7c293c3d66
Andres Sardou <docsardou@gmail.com>
2019-04-15 09:40:11 -0400

    add auto-refreash

--- js/script.js
+++ js/script.js
@@ -2,36 +2,43 @@
 Array of Quotes + Author + Citation + Year  
 Quotes found on https://www.bustle.com/articles/121359-25-of-the-best-book-quotes-of-all-time
 */
+
 var quotes = [{
     Quote: "It's the possibility of having a dream come true that makes life interesting.",
     Author: 'Paulo Coelho',
     Citation: 'The Alchemist',
-    Year: '1988'
+    Year: '1988',
+    Category: 'Fantasy'
 }, {
     Quote: "I cannot fix on the hour, or the spot, or the look or the words, which laid the foundation. It is too long ago. I was in the middle before I knew that I had begun.",
     Author: 'Jane Austen',
     Citation: 'Pride and Prejudice',
-    Year: '1797'
+    Year: '1797',
+    Category: 'Novel of manners'
 }, {
     Quote: 'There is no greater agony than bearing an untold story inside you.',
     Author: 'Maya Angelou',
     Citation: ' I Know Why the Caged Bird Sings',
-    Year: '1968'
+    Year: '1968',
+    Category: 'Autobiography'
 }, {
     Quote: 'Still, there are times I am bewildered by each mile I have traveled, each meal I have eaten, each person I have known, each room in which I have slept. As ordinary as it all appears, there are times when it is beyond my imagination.',
     Author: 'Jhumpa Lahiri',
     Citation: 'Interpreter of Maladies',
-    Year: '1999'
+    Year: '1999',
+    Category: 'Short stories'
 }, {
     Quote: "And so we beat on, boats against the current, borne back ceaselessly into the past.",
     Author: "F. Scott Fitzgerald",
     Citation: 'The Great Gatsby',
-    Year: '1924'
+    Year: '1924',
+    Category: 'Novel'
 }, {
     Quote: "Hello, babies. Welcome to Earth. It's hot in the summer and cold in the winter. It's round and wet and crowded. At the outside, babies, you've got about a hundred years here. There's only one rule that I know of, babies—God damn it, you've got to be kind.",
     Author: "Kurt Vonnegut",
     Citation: "God Bless You, Mr. Rosewater",
-    Year: '1965'
+    Year: '1965',
+    Category: 'Novel'
 }];
 
 // Funtion that generates a random Quote from the quotes array
@@ -54,7 +61,7 @@ function random_bg_color() {
 
 /*
 Funtion that creates an HTML string of a random quote. 
-Quotes also containing Citation, Year, or both are tested with 'IF' statements. 
+Quotes also containing Citation, Year, or Category are tested with 'IF' statements. 
 Calls random background function with each random quote generated. 
 */
 
@@ -69,6 +76,9 @@ function printQuote() {
     if (randomQuote.Year) {
         HTMLmessage += '<span class="year">' + randomQuote.Year + '</span>';
     }
+    if (randomQuote.Category) {
+        HTMLmessage += '<span class="category">' + randomQuote.Category + '</span>';
+    }
     HTMLmessage += '</p>';
     var div = document.getElementById('quote-box');
     div.innerHTML = HTMLmessage;
@@ -76,6 +86,11 @@ function printQuote() {
     random_bg_color();
     return HTMLmessage;
 }
+
+// Auto-refreashes the page after 20 seconds (20000 milliseconds)
+
+var pageRefreash = window.setInterval(printQuote, 20000);
+
 /*
 Displays a new quote on page when use clicks "Show another quote".
 */
